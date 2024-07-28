edd4978d12844394b8fe5c3e9659dfdc7a2082ea
Damigrace1 <ogdamtim@gmail.com>
2024-07-28 20:35:38 +0100

    Business card created

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tapcard/views/widgets/business_card.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: Center(
+        child: BusinessCard(),
+      ),
+    );
   }
 }
