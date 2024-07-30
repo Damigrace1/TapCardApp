58abce1d36dd1083ed5f59b6764b17a744ad4d41
Tosin Ezekiel <tosinezekiel8@gmail.com>
2024-07-30 01:18:35 +0100

    Update mycontacts.dart

--- lib/views/home/home_tabs/mycontacts.dart
+++ lib/views/home/home_tabs/mycontacts.dart
@@ -5,8 +5,29 @@ class ContactsTab extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Contacts'),
+    return Scaffold(
+      backgroundColor: Colors.grey[100],
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15.0, right: 15.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20,), 
+              TextFormField(
+                
+                decoration: const InputDecoration(
+                  fillColor: Colors.white, 
+                  filled: true,
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(15))),
+                  hintText: 'Search'
+                ),
+              )
+            ],
+          ),
+        )
+      ),
     );
   }
 }
